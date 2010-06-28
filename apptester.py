1a70c60e5d983f2dd015803b5441f486bdc4c808
Seth Shelnutt <Shelnutt2@gmail.com>
2010-06-28 21:05:01 +0000

    Version 0.1

--- apptester.py
+++ apptester.py
@@ -16,6 +16,42 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
 
-import os, os.path, string, sys
+import os, os.path, string, sys, tempfile, xml.dom.minidom
+from optparse import OptionParser
+
+
+
+parser = OptionParser(usage="usage: %prog [options] arg", version="%prog 0.1")
+parser.add_option("-D", "--debug", action="store_true", dest="debug",
+		 help="Set whether to print debug info or not")
+parser.add_option("-A", "-a", "--All", "--all", action="store_true", dest="All",
+		 help="Flag sets whether to run all tests or only the packages specified in args")
+parser.add_option("-l", "-L", "--logfile", dest="logfilename",
+		 help="specify location of the logfile else log file is " + tempfile.gettempdir() + "/apptester.log")
+
+
+(options, args) = parser.parse_args()
+if len(args) != 1:
+    parser.error("Please either use the -A flag or specify a specific software package")
+
+if 'options.logfilename' in globals():
+   pass
+else:
+   options.logfilename = os.path.join( tempfile.gettempdir(), "apptester.log" ) 
+if options.debug:
+ if os.path.isfile( options.logfilename ):
+   os.remove(options.logfilename)
+ class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = []
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.append(message)  
+
+sys.stdout = Logger()
+
+
 
 
